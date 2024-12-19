30f542b1f2c9cbf128956d208558f27bcb260b4a
Harald Steinlechner <haraldsteinlechner@gmail.com>
2024-12-19 15:26:23 +0100

    implemented undo for canvas example

--- src/Canvas.fs
+++ src/Canvas.fs
@@ -11,24 +11,39 @@ type Circle =
         pos: Coord
     }
 
-type Model = Circle list
+type Model = { 
+    circles : list<Circle>  
+    past : Option<Model>
+}
 
 type Msg =
     | AddPoint of Coord
+    | Undo
 
 let init () =
-    [
-        { pos = { x = 50.0; y = 50.0 } }
-        { pos = { x = 150.0; y = 150.0 } }
-        { pos = { x= 100.0; y = 100.0 } }
-    ]
+    { 
+        past = None
+        circles = 
+            [
+                { pos = { x = 50.0; y = 50.0 } }
+                { pos = { x = 150.0; y = 150.0 } }
+                { pos = { x= 100.0; y = 100.0 } }
+            ]
+    }
     , Cmd.none
 
 
 let update (msg : Msg) (currentModel : Model)  =
     match msg with
     | AddPoint p -> 
-        { pos = p; } :: currentModel, Cmd.none
+        { currentModel with 
+            circles = { pos = p; } :: currentModel.circles 
+            past = Some currentModel
+        }, Cmd.none
+    | Undo ->
+        match currentModel.past with
+        | None -> currentModel, Cmd.none
+        | Some p -> p, Cmd.none
 
 
 [<Emit("getSvgCoordinates($0)")>]
@@ -37,7 +52,7 @@ let getSvgCoordinates o: Coord = jsNative
 let render (model : Model) (dispatch : Msg -> unit) =
      
     let points = 
-        model |> List.mapi (fun index point -> 
+        model.circles |> List.mapi (fun index point -> 
             Svg.circle [
                 svg.cx point.pos.x
                 svg.cy point.pos.y
@@ -64,13 +79,10 @@ let render (model : Model) (dispatch : Msg -> unit) =
 
     Html.div [
         prop.children [
-            Html.h1 "Simplest drawing"
+            Html.h1 "Simplest CANVAS..."
             Html.button [
                 prop.style [style.margin 20]; 
-                prop.children [Html.text "undo"]
-            ]
-            Html.button [
-                prop.style [style.margin 20]; 
+                prop.onClick (fun _ -> dispatch Undo)
                 prop.children [Html.text "undo"]
             ]
             Html.br []
